d8a3ec89a220d5cc2a8c2d3a95ad75d242dab10b
Av23037 <av23037@ues.edu.sv>
2026-04-07 17:26:02 -0600

    hospital

---
+++ backend/hospital/hospital/src/main/java/com/hospital/config/OpenAPIConfig.java
@@ -0,0 +1,21 @@
+package com.hospital.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sistema Hospitalario API")
+                        .version("1.0")
+                        .description("Gestión de pacientes")
+                        .contact(new Contact().name("Grupo")));
+    }
+}
